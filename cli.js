d3234290131af0252d943ff6c8242df2c47641b8
Joe Hand <joe@joeahand.com>
2016-11-04 16:15:56 -0700

    basic publisher with hypercore-archiver

--- cli.js
+++ cli.js
@@ -4,52 +4,16 @@ var crypto = require('crypto')
 var datPublish = require('.')
 
 var args = require('minimist')(process.argv.splice(2), {
-  alias: {
-    d: 'dat-download', // dat discovery + download (updates archive via dat)
-    u: 'dat-upload', // dat discovery + upload
-    h: 'http',
-    r: 'root',
-    i: 'index'
-  },
-  boolean: ['dat-download', 'dat-upload', 'http', 'webrtc'],
-  default: {
-    port: 8080,
-    http: true
-  }
+  // alias: {
+  //   d: 'dat-download', // dat discovery + download (updates archive via dat)
+  //   u: 'dat-upload', // dat discovery + upload
+  //   h: 'http'
+  // },
+  // boolean: ['dat-download', 'dat-upload', 'http']
 })
 
-if (args.webrtc) {
-  try {
-    var webrtc = require('electron-webrtc')()
-  } catch (e) {
-    console.error('npm install electron-webrtc to use webrtc option')
-    process.exit(1)
-  }
-}
-
-
-var server = http.createServer()
-var publish = datPublish({
-  dir: args._[1], // TODO: fix for using without key arg
-  http: args.http,
-  discovery: {
-    upload: args['dat-upload'],
-    download: args['dat-download']
-  },
-  rootArchive: args.root,
-  index: args.index,
-  webrtc: webrtc
-})
+var publish = datPublish()
 
 var key = args._[0] || crypto.randomBytes(16).toString('hex')
 publish.archiver.join(key)
 console.log(`dat-archiver listening on ${key}`)
-
-if (args.http) {
-  server.listen(args.port)
-  server.once('error', function () {
-    server.listen(0)
-  })
-  server.on('request', publish.httpRequest)
-  console.log(`hyperdrive-http listening on ${args.port}`)
-}
--- index.js
+++ index.js
@@ -1,105 +1,17 @@
 var fs = require('fs')
 var path = require('path')
-var mkdirp = require('mkdirp')
 var Archiver = require('dat-archiver')
-var hyperdriveHttp = require('hyperdrive-http')
-var Dat = require('dat-js')
-var level = require('level-party')
+var serveArchives = require('archiver-server')
 
 module.exports = function (opts) {
   if (!opts) opts = {}
-  var datsDir = opts.dir || path.join(process.cwd(), 'dats')
-  var discovery = opts.discovery || false
-  if (!discovery.upload && !discovery.download) discovery = false
-  var shareHttp = opts.http || true
-  var rootArchive = opts.rootArchive || false
-  opts.index = opts.index && typeof opts.index !== 'string' ? 'index.html' : opts.index
-  var indexPage = rootArchive && opts.index ? opts.index : false
-  var openDats = {}
+  var dir = opts.dir || path.join(process.cwd(), 'dats')
 
-  if (rootArchive && typeof rootArchive !== 'string') {
-    // check if we have mulitple folders
-    try {
-      var dats = fs.readdirSync(datsDir)
-      var count = 0
-      dats.forEach(function (dat) {
-        if (fs.statSync(path.join(datsDir, dat)).isDirectory()) {
-          count++
-          if (count > 1) throw new Error('More than one dat found. Specify key for root archive mode.')
-          rootArchive = dat
-        }
-      })
-    } catch (_) {
-      // no dats yet, set archive key later
-    }
-  }
+  var archiver = Archiver({dir: dir})
 
-  var archiver = Archiver({dir: datsDir, getArchive: function (key, cb) {
-    getArchive({key: key, type: 'archiver'}, cb)
-  }})
-  var onrequest = hyperdriveHttp(function (datInfo, cb) {
-    if (!rootArchive && !datInfo.key) return cb(new Error('Not found'))
-    if (rootArchive && !datInfo.key) datInfo.key = rootArchive
-    if (rootArchive && indexPage) {
-      if (!datInfo.filename) datInfo.filename = indexPage
-      else if (datInfo.filename === 'metadata.json') datInfo.filename = null
-    }
-    getArchive({key: datInfo.key, type: 'http'}, cb)
-  })
-
-  archiver.on('connection', function () {
-    console.log('New push connection.')
-  })
-
-  archiver.on('key-received', function (key) {
-    console.log('Push started. Key:', key)
-  })
-
-  archiver.on('archive-finished', function (key) {
-    console.log('Finished receiving push. Key:', key)
-    if (discovery) openDats[key]._joinSwarm() // TODO: I guess this makes sense?
-    else openDats[key].db.close()
-  })
-
-  archiver.on('error', function (err) {
-    console.error(err)
-  })
+  serveArchives(archiver.archives, opts)
 
   return {
-    archiver: archiver,
-    httpRequest: shareHttp ? onrequest : undefined
-  }
-
-  function getArchive (opts, cb) {
-    var key = opts.key
-    var type = opts.type
-    if (typeof key !== 'string') key = key.toString('hex')
-
-    var dir = path.join(datsDir, key)
-    try {
-      if (!fs.statSync(dir).isDirectory() && type === 'http') return cb(new Error('No Archive found for http', key))
-    } catch (e) { if (type === 'http') return cb(new Error('No Archive found for http', key)) }
-    if (type === 'archiver') mkdirp.sync(dir)
-
-    var dat = openDats[key]
-    if (dat) return done()
-
-    dat = Dat({
-      dir: dir,
-      key: key,
-      db: level(path.join(dir, '.dat')),
-      discovery: discovery,
-      webrtc: opts.webrtc
-    })
-    dat.open(function (err) {
-      if (err) return cb(err)
-      openDats[key] = dat
-      if (rootArchive && typeof rootArchive !== 'string') rootArchive = key
-      done()
-    })
-
-    function done () {
-      cb(null, dat.archive)
-    }
+    archiver: archiver
   }
 }
